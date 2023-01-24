e1532ffe9a0c0c22d092b823e16d45a155f32bc8
Muqads-Riaz <riazahmad03017@gmail.com>
2023-01-24 15:46:06 +0500

    1st Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Calendar from './Components/Calendar/MainScreen';
+
+
+ function App() {
+  return (
+    <Calendar style={{ position: "relative", margin: "50px auto"}} width="300px" /> 
+  )
+}
+export default App;
